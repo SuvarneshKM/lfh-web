427945479a1b51e4022247080ac6e2e24532daeb
Suvarnesh K M <suvarnesh1729@gmail.com>
2021-05-16 15:39:43 +0530

    final

---
+++ script.js
@@ -0,0 +1,21 @@
+const sections = document.querySelectorAll("section");
+const navA = document.querySelectorAll("header nav ul li a");
+
+window.addEventListener('scroll', ()=>{
+    let current = '';
+    sections.forEach( section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        if(pageYOffset >= sectionTop - sectionHeight / 3){
+            current = section.getAttribute('id');
+        }
+    })
+
+    navA.forEach( a => {
+        a.classList.remove('active');
+        if(a.classList.contains(current)){
+            a.classList.add('active')
+        }
+    })
+})
+
